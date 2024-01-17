d4b667ef9d31aaa8425f6f2015d872edcc6af4e4
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2024-01-17 12:12:44 +0200

    Tasks 1-2

--- src/2.ts
+++ src/2.ts
@@ -16,23 +16,21 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  private department: string;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(public name: string, protected salary: number) {
   }
 
-  getEmployeeDetails() {
+  public getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, salary: number) {
+    super(name, salary += 10000);
+  }
 }
 
 
