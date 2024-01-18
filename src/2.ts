d3e1cd79e2cd603500b33c1ba712baff49cda783
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2024-01-18 11:30:06 +0200

    Summary

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,8 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  private department: string;
 
-  constructor(public name: string, protected salary: number) {
+  constructor(public name: string, private department: string, protected salary: number) {
   }
 
   public getEmployeeDetails() {
@@ -28,8 +27,8 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
-  constructor(name: string, salary: number) {
-    super(name, salary += 10000);
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary += 10000);
   }
 }
 
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,48 @@
+class Key{
+    private signature: number = Math.random();
+
+    getSignature() : number {
+        return this.signature;
+    }
+};
+
+class Person{
+    constructor(public key: Key) { }
+    
+    getKey(): number{
+        return key.getSignature();
+    }
+}
+
+abstract class House{
+    protected door: true | false = false;
+    protected tenants: Person[] = [];
+
+    constructor(protected key: Key){}
+
+    comeIn(person: Person) {
+        if (this.door === true) {
+            this.tenants.push(person);
+        }
+        
+    };
+    abstract openDoor(trueKey: number): void;
+};
+
+class MyHouse extends House{
+
+    constructor(key: Key) {
+        super(key)
+    }
+    openDoor(key: number) {
+        if (this.key.getSignature() === key) {
+            this.door = true;
+        }
+        this.door = false;
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
